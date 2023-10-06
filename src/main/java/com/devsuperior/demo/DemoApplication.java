9a9ea862cfcb54794448ab4db85cb03819ab9253
LopezTw <caiohg_94@hotmail.com>
2023-10-06 17:21:02 -0300

    BCrypt password

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/*
+	 * @Autowired private PasswordEncoder passwordEncoder;
+	 */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	/*
+	 * @Override public void run(String... args) throws Exception {
+	 * System.out.println("CRIPTOGRAFADO:" + passwordEncoder.encode("123456")); }
+	 */
 
 }
--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,16 +3,23 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+	@Bean
+	PasswordEncoder getPasswordEncoder() { // utiliza a interface pra maleabilidade se fosse mudar o BCrypt pra outro tipo de Criptografia
+		return new BCryptPasswordEncoder();
+	}
+	
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 		return http.build();
 	}
-
+    
 }
